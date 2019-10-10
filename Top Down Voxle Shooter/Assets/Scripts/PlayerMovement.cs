de50a459b7c15fef9850357777ad9c1f1f3b966c
Julien <minecarman53@gmail.com>
2019-10-10 15:18:08 -0400

    Added Player Floating

--- Top Down Voxle Shooter/Assets/Scripts/PlayerMovement.cs
+++ Top Down Voxle Shooter/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 10f;
+    public float floatHeight  = 1;
+
+    private Rigidbody rb;
+
+    void Start() 
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     void Update() 
     {
@@ -12,5 +20,19 @@ public class PlayerMovement : MonoBehaviour
         float inputZ = Input.GetAxis("Vertical");
 
         transform.Translate(inputX * Time.deltaTime * speed, 0, inputZ * Time.deltaTime * speed);
+    }
+
+    void FixedUpdate() 
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, Vector3.down, out hit, floatHeight)) 
+        {
+            transform.position = hit.point + new Vector3(0, floatHeight, 0);
+            rb.useGravity = false;
+        }
+        else 
+        {
+            rb.useGravity = true;
+        }   
     }
 }
