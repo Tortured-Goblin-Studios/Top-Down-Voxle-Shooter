2c888b53ffd5b52df4d377b83b0026161d30b93c
Julien Dewolfe <minecarman53@gmail.com>
2019-10-11 11:15:00 -0400

    Added Player Rotation

--- Top Down Voxle Shooter/Assets/Scripts/PlayerMovement.cs
+++ Top Down Voxle Shooter/Assets/Scripts/PlayerMovement.cs
@@ -16,23 +16,49 @@ public class PlayerMovement : MonoBehaviour
 
     void Update() 
     {
-        float inputX = Input.GetAxis("Horizontal");
-        float inputZ = Input.GetAxis("Vertical");
-
-        transform.Translate(inputX * Time.deltaTime * speed, 0, inputZ * Time.deltaTime * speed);
+        Move();
+        Rotate();
     }
 
     void FixedUpdate() 
     {
+        Hover();
+    }
+
+    void Hover()
+    {
         RaycastHit hit;
-        if(Physics.Raycast(transform.position, Vector3.down, out hit, floatHeight)) 
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, floatHeight))
         {
             transform.position = hit.point + new Vector3(0, floatHeight, 0);
             rb.useGravity = false;
         }
-        else 
+        else
         {
             rb.useGravity = true;
-        }   
+        }
+    }
+
+    void Move()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputZ = Input.GetAxis("Vertical");
+
+        transform.Translate(inputX * Time.deltaTime * speed, 0, inputZ * Time.deltaTime * speed, Space.World);
+    }
+
+    void Rotate()
+    {
+        Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+
+        if(groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue);
+
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+        }
     }
 }
