1ad976a1779d28f9b842a4db79f2eac897b3ae81
Quinn <mayorbuddy@icloud.com>
2019-10-16 16:51:30 -0400

    Added shooting capebilitys

---
+++ Top Down Voxle Shooter/Assets/DeleteBulletOnImpact.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteBulletOnImpact : MonoBehaviour
+{
+    public float destroyTime;
+    private float delay = 0;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        delay += Time.deltaTime;
+        if(delay <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
