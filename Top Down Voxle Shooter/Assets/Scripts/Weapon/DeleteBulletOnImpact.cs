47785b08c4626df57dce3e1aa5617a166883a6f0
Julien Dewolfe <minecarman53@gmail.com>
2019-10-16 17:03:21 -0400

    Cleaned Up Folder Tree

---
+++ Top Down Voxle Shooter/Assets/Scripts/Weapon/DeleteBulletOnImpact.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteBulletOnImpact : MonoBehaviour
+{
+    public float destroyTime;
+
+    void Start()
+    {
+        Destroy(gameObject, destroyTime);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
