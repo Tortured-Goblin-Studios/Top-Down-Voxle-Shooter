becb8c91d8b716fcb0396a634de632f684b9848b
Quinn <mayorbuddy@icloud.com>
2019-10-22 15:13:36 -0400

    added Enemy and Player health

---
+++ Top Down Voxle Shooter/Assets/EnemyHealth.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float enemyStartHealth;
+    private float enemyHealth;
+    public Transform enemySpawnPoint;
+
+    void Start()
+    {
+        enemyHealth = enemyStartHealth;
+        enemySpawnPoint = GameObject.Find("Enemy Spawn Point").transform;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Bullet")
+        {
+            enemyHealth -= 1;
+            if (enemyHealth <= 0)
+            {
+                Respawn();
+            }
+        }
+    }
+
+    public void Respawn()
+    {
+        transform.position = enemySpawnPoint.position;
+        enemyHealth = enemyStartHealth;
+    }
+}
