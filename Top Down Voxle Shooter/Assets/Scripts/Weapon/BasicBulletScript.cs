52294586c504bb60ac727632d67f153d244ef5bb
Julien <minecarman53@gmail.com>
2019-11-19 13:48:08 -0500

    Cleaned Up File Structure

--- Top Down Voxle Shooter/Assets/Scripts/Weapon/BasicBulletScript.cs
+++ Top Down Voxle Shooter/Assets/Scripts/Weapon/BasicBulletScript.cs
@@ -7,8 +7,19 @@ public class BasicBulletScript : MonoBehaviour
     public Rigidbody bulletRB;
     public float moveSpeed;
 
+    public bool isArena;
+
     void Update()
     {
         transform.Translate(transform.forward * Time.deltaTime * moveSpeed, Space.World);
+    }
+
+    void OnCollisionEnter(Collision col) {
+        Debug.Log("BulletHit");
+        if(isArena) {
+            
+            if(col.transform.GetComponent<ArenaPlayerHealth>())
+                col.transform.GetComponent<ArenaPlayerHealth>().TakeDamage(1);
+        }
     }
 }
